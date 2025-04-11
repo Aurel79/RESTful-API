0ffacdf10f77db5ea3c957cd561791baf4e1341e
Aurel79 <faurelia795@gmail.com>
2025-04-11 18:05:38 +0700

    add authentication with JWT token

---
+++ app/Http/Controllers/AuthController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
+use Tymon\JWTAuth\Facades\JWTAuth;
+use Tymon\JWTAuth\Exceptions\JWTException;
+
+class AuthController extends Controller
+{
+    public function login(Request $request)
+    {
+        $credentials = $request->only('email', 'password');
+
+        try {
+            if (! $token = JWTAuth::claims(['role' => 'user'])->attempt($credentials)) {
+                return response()->json(['error' => 'Invalid credentials'], 401);
+            }
+            return response()->json([
+                'access_token' => $token,
+                'token_type' => 'bearer',
+                'expires_in' => JWTAuth::factory()->getTTL() * 60
+            ]);
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Could not create token'], 500);
+        }
+    }
+
+    public function getUser()
+    {
+        try {
+            if (! $user = JWTAuth::parseToken()->authenticate()) {
+                return response()->json(['error' => 'User not found'], 404);
+            }
+        } catch (JWTException $e) {
+            return response()->json(['error' => 'Invalid token'], 400);
+        }
+
+        return response()->json(compact('user'));
+    }
+
+    public function logout()
+    {
+        JWTAuth::invalidate(JWTAuth::getToken());
+        return response()->json(['message' => 'Successfully logged out']);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,21 @@
-<?php 
+<?php
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\StudentController;
+use App\Http\controllers\AuthController;
+use App\Http\Middleware\JwtMiddleware;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
   return $request->user();
 });
 
+
 Route::apiResource('/students', StudentController::class);
+
+Route::withoutMiddleware('jwtAuth')->group(function () {
+  Route::post('login', [AuthController::class, 'login']);
+});
+Route::get('user', [AuthController::class, 'getUser']);
+Route::post('logout', [AuthController::class, 'logout']);
+
